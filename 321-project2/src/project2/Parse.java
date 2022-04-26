63bad4c0d3e57235ab2dc868b4c68e74cb6a585e
Colin Brenizer <cbren@iastate.edu>
2022-04-26 13:18:08 -0500

    initial commit

---
+++ 321-project2/src/project2/Parse.java
@@ -0,0 +1,45 @@
+package project2;
+import java.io.*;
+
+public class Parse {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		String inputFile;
+		try {
+		inputFile = args[0];
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("Please enter a binary file to parse");
+			return;
+		}
+		
+		int byte_read = 0;
+		try {
+			InputStream inputStream = new FileInputStream("assignment1.machine");
+			while(inputStream.available() > 0)
+			{
+				byte_read = inputStream.read();
+				String binary = String.format("%"+8+"s",Integer.toBinaryString(byte_read))
+					.replaceAll(" ", "0");
+				
+				byte_read = inputStream.read();
+				binary += String.format("%"+8+"s",Integer.toBinaryString(byte_read))
+					.replaceAll(" ", "0");
+				
+				byte_read += inputStream.read();
+				binary += String.format("%"+8+"s",Integer.toBinaryString(byte_read))
+					.replaceAll(" ", "0");
+				
+				byte_read = inputStream.read();
+				binary += String.format("%"+8+"s",Integer.toBinaryString(byte_read))
+					.replaceAll(" ", "0");
+				System.out.println(binary);
+				inputStream.skip(4);
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
